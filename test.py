dacfe623f09db75751f84c4508992ecbf68ea240
Fahd <fahd31256@gmail.com>
2023-12-13 10:08:43 +0000

    chore: update test image

--- test.py
+++ test.py
@@ -11,13 +11,23 @@ with torch.no_grad():
 	predictions = model(low)
 
 plt.figure(figsize=(4, 10))
+
 for i in range(0, 12, 3):
 	plt.subplot(4, 3, i+1)
+	if i == 0:
+		plt.title('Input Image')
+	plt.axis('off')
 	plt.imshow(T.ToPILImage()(low[i]))
 
 	plt.subplot(4, 3, i+2)
+	if i == 0:
+		plt.title('Ground Truth')
+	plt.axis('off')
 	plt.imshow(T.ToPILImage()(high[i]))
 
 	plt.subplot(4, 3, i+3)
+	if i == 0:
+		plt.title('AI Denoised')
+	plt.axis('off')
 	plt.imshow(T.ToPILImage()(predictions[i]))
 plt.show()
